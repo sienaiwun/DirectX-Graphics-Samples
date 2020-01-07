848250793c3b76ec18ed8a9e2cf8e3689802400a
sienaiwun <sienaiwun@gmail.com>
2020-01-07 11:25:44 +0800

    reset counter

--- MiniEngine/MultiThread/MultiThread.cpp
+++ MiniEngine/MultiThread/MultiThread.cpp
@@ -170,8 +170,11 @@ void MultiThread::RenderScene(void)
     ComputeContext& computeContext = gfxContext.GetComputeContext();
     computeContext.SetRootSignature(s_ComputeSig);
     computeContext.SetPipelineState(s_ComputePSO);
-    computeContext.ResetCounter(s_precessedCommandBuffer,0);
+    static __declspec(align(16)) UINT zero = 0;
+    gfxContext.TransitionResource(s_precessedCommandBuffer, D3D12_RESOURCE_STATE_COPY_DEST);
+    computeContext.WriteBuffer(s_precessedCommandBuffer.GetCounterBuffer(), 0, &zero, sizeof(zero));//reset counter
     computeContext.TransitionResource(s_commandBuffer, D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE);
+    computeContext.TransitionResource(s_precessedCommandBuffer, D3D12_RESOURCE_STATE_UNORDERED_ACCESS, true);
     computeContext.SetDynamicDescriptor(ComputeRootParams::UAVParam, 0, s_precessedCommandBuffer.GetUAV());
     computeContext.SetDynamicDescriptor(ComputeRootParams::StructBufferParam, 0, s_commandBuffer.GetSRV());
     computeContext.SetDynamicConstantBufferView(ComputeRootParams::UniformBufferParam, sizeof(gUniformData), &gUniformData);
